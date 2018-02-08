71496bb1f76f1d2816956cae55ae6e55b5c58507
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-08 08:29:18 -0700

    set up bootstrap challenge index file

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+	<meta charset="utf-8" />
+	<head>
+		<title>bootstrap challenge</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+		<!--BOOTSTRAP CSS-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!--Other and custom CSS-->
+
+		<!--Optional Javascript links-->
+		<!--link to Bootstrap javascript dependencies-->
+		<!--(Put it here, but if it is a heavy site, put this block just below the closing body tag)-->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</head>
+
+	<body>
+		<header>
+		<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-dark text-white">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+					<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+					<a class="nav-item nav-link" href="#">Features</a>
+					<a class="nav-item nav-link" href="#">Pricing</a>
+					<a class="nav-item nav-link disabled" href="#">Disabled</a>
+				</div>
+			</div>
+		</nav>
+		</div>
+			</header>
+		<main>
+			<div class="container">
+
+		</div>
+		</main>
+		<footer>
+			<div class="container">
+
+		</div>
+		</footer>
+
+		<h1></h1>
+
+	</body>
+
+
+
+
+</html>
